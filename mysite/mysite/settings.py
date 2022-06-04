a2d2ca3a1e5461a8709ac573b8cc5e27f1fc8f37
huyuneko0212 <98094956+huyuneko0212@users.noreply.github.com>
2022-06-04 22:25:03 +0900

    設定追加

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,5 +131,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 #STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
